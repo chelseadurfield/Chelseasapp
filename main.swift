108e601970c7c0af89cf8218d633cf9370abee52
chelseadurfield <chelseadurfield@gmail.com>
2024-07-11 14:14:12 -0400

    Updated main.swift

---
+++ main.swift
@@ -0,0 +1,30 @@
+// Online Swift compiler to run Swift program online
+// Print "Try programiz.pro" message
+print("Try programiz.pro")
+//constants
+let AppName: String = "Chelsea's app"
+let Developername: String = "Chelsea"
+let Intialreleaseyear: Int = 2024
+
+//variable
+var CurrentVersion: Double = 15.5
+var Numberofdownloads: Int = 10000
+var Userrating: Double = 4.5
+
+// Print inital data
+print("App Name: \(AppName)")
+print("Developer Name: \(Developername)")
+print("Inital Release Year: \(Intialreleaseyear)")
+print("Current Version:\(CurrentVersion)")
+print("Number of Downloads:\(Numberofdownloads)")
+print("User Rating:\(Userrating)")
+
+CurrentVersion += 0.1
+print("Current Version:\(CurrentVersion)")
+Numberofdownloads += 1000
+print("Number of Downloads:\(Numberofdownloads)")
+Userrating += 0.2
+print("User Rating:\(Userrating)")
+print("Updated Current Version:\(CurrentVersion)")
+print("Updated Number of Downloads:\(Numberofdownloads)")
+print("Updated User Rating:\(Userrating)")
